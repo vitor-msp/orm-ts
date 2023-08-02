f6da72179c08af97a94cabfa2f91cb0a20fae015
Vitor Mateus <vitor7jan@gmail.com>
2023-08-02 16:34:14 -0300

    Registry unit tests to update

--- src/domain/Registry.spec.ts
+++ src/domain/Registry.spec.ts
@@ -30,4 +30,31 @@ describe("unit tests for Registry class", () => {
     const data: any = {};
     expect(() => RegistryBuilder.build(schema, data, 1)).toThrow(Error);
   });
+
+  test("ensure update a registry", () => {
+    const data: any = {
+      name: "registry",
+      age: 24,
+    };
+    const registry = RegistryBuilder.build(schema, data, 1);
+    RegistryBuilder.update(
+      schema,
+      { name: "edited", other: "ignored" },
+      registry
+    );
+    expect(registry.id).toEqual(1);
+    expect(registry.name).toEqual("edited");
+    expect(registry.age).toEqual(24);
+  });
+
+  test("ensure throw error when age is invalid", () => {
+    const data: any = {
+      name: "registry",
+      age: 24,
+    };
+    const registry = RegistryBuilder.build(schema, data, 1);
+    expect(() =>
+      RegistryBuilder.update(schema, { age: "edited" }, registry)
+    ).toThrow(Error);
+  });
 });
