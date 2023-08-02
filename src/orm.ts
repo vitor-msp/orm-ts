df6e6f949358440a29dedfc75fd36d094a109439
Vitor Mateus <vitor7jan@gmail.com>
2023-08-02 11:24:04 -0300

    added tests

---
+++ src/orm.ts
@@ -0,0 +1,58 @@
+
+
+///////////////// Tests /////////////////
+
+(() => {
+  orm
+    .createTable("products")
+    .field({ name: "id", type: "number", nullable: false, pk: true })
+    .field({ name: "name", type: "text", nullable: false })
+    .field({ name: "value", type: "number", nullable: false })
+    .field({ name: "unit", type: "text", nullable: false });
+  console.log(orm.getTable("products"));
+})();
+/* returns: 
+{
+    tableName: "products",
+    fields: [
+        { name: "id", type: "number", nullable: false, pk: true },
+        { name: "name", type: "text", nullable: false },
+        { name: "value", type: "number", nullable: false },
+        { name: "unit", type: "text", nullable: false }
+    ]
+}
+*/
+console.log(
+  orm.table("products").insert({ name: "apple", value: 5.49, unit: "kg" })
+);
+// returns: 1 line affected
+console.log(
+  orm
+    .table("products")
+    .select(1, { id: true, name: true, value: true, unit: true })
+);
+// returns: { id: 1, name: "apple", value: 5.49, unit: "kg" }
+console.log(
+  orm
+    .table("products")
+    .select(1, { id: false, name: true, value: false, unit: true })
+);
+// returns: { name: "apple", unit: "kg" }
+console.log(orm.table("products").update(1, { name: "banana", value: 2.49 }));
+// returns: 1 line affected
+console.log(
+  orm
+    .table("products")
+    .select(1, { id: true, name: true, value: true, unit: true })
+);
+// returns: { id: 1, name: "banana", value: 2.49, unit: "kg" }
+console.log(
+  orm
+    .table("products")
+    .select(1, { id: false, name: true, value: true, unit: false })
+);
+// returns: { name: "banana", value: 2.49 }
+console.log(orm.table("products").delete(1));
+// returns: 1 line affected
+console.log(orm.table("products").selectMany());
+// returns: []
